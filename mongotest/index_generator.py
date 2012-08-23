b901163233f0aab72dda792ea9ec8d9b6c2dbc4f
Nathan West <Lucretiel@gmail.com>
2012-08-23 15:25:50 -0400

    Added mongoles module, started IndexGenerator

---
+++ mongotest/index_generator.py
@@ -0,0 +1,20 @@
+import random
+
+class IndexGenerator:
+	def __init__(self):
+		#mime_types is a dict of all the possible mime types. Each type is
+		#associated with a file extension (.gif, etc)
+		self.mime_types = 
+		{
+			"image":
+			{
+				"gif": ".gif",
+				"jpeg": ".jpg",
+				"png": ".ping",
+				"tiff": ".tiff"
+			}
+			"audio":
+			{
+				""
+			}
+		}
