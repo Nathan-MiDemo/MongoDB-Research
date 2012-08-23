046afbcb3de936fe9515821407d31689ecb1e174
Nathan West <Lucretiel@gmail.com>
2012-08-23 16:43:38 -0400

    Unit tests and random mime generator
    Completed the IndexGenerator.mime_types dict
    Wrote random_type_generator
    Wrote unit tests

--- mongotest/index_generator.py
+++ mongotest/index_generator.py
@@ -1,20 +1,56 @@
 import random
 
 class IndexGenerator:
-	def __init__(self):
-		#mime_types is a dict of all the possible mime types. Each type is
-		#associated with a file extension (.gif, etc)
-		self.mime_types = 
+	mime_types = {
+		"image":
 		{
-			"image":
-			{
-				"gif": ".gif",
-				"jpeg": ".jpg",
-				"png": ".ping",
-				"tiff": ".tiff"
-			}
-			"audio":
-			{
-				""
-			}
+			"gif": ".gif",
+			"jpeg": ".jpg",
+			"png": ".ping",
+			"tiff": ".tiff"
+		},
+		"audio":
+		{
+			"mp4": ".mp4",
+			"mpeg": ".mp3",
+			"ogg": ".ogg",
+			"vnd.wave": ".wav"
+		},
+		"video":
+		{
+			"mpeg": ".mpg",
+			"mp4": ".mp4",
+			"quicktime": ".mov",
+			"x-ms-wmv": ".wmv"
+		},
+		"application":
+		{
+			"json": ".json",
+			"pdf": ".pdf",
+			"javascript": ".js",
+			"postcript": ".ps",
+			"zip": ".zip",
+			"x-gzip": ".gz"
+		},
+		"text":
+		{
+			"css": ".css",
+			"csv": ".csv",
+			"html": ".html",
+			"plain": ".txt",
+			"xml": ".xml"
 		}
+	}
+
+	def __init__(self):
+		pass
+
+
+	def random_mime_type(self, type=''):
+		if type is '':
+			type = random.choice(self.mime_types.keys())
+
+		subtype = random.choice(self.mime_types[type].keys())
+
+		#returns (complete-mime-type, extension)
+		return (type, subtype), self.mime_types[type][subtype]
