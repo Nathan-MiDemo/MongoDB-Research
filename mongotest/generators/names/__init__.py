00f680cb673ba3abc98b8ad5eeba9b03fe6ddfda
Nathan West <Lucretiel@gmail.com>
2012-08-24 13:23:15 -0400

    Massive refactoring of project code
    Moved everything to various modules. Removed test code, to be re-added
    later. Removed index_generator, for now.

---
+++ mongotest/generators/names/__init__.py
@@ -0,0 +1,1 @@
+#This module contains all functions for generating strings and names
---
+++ mongotest/generators/names/files.py
@@ -0,0 +1,73 @@
+from . import generate_name
+
+mime_types = {
+		"image":
+		{
+			"gif": "gif",
+			"jpeg": "jpg",
+			"png": "ping",
+			"tiff": "tiff"
+		},
+		"audio":
+		{
+			"mp4": "mp4",
+			"mpeg": "mp3",
+			"ogg": "ogg",
+			"vnd.wave": "wav"
+		},
+		"video":
+		{
+			"mpeg": "mpg",
+			"mp4": "mp4",
+			"quicktime": "mov",
+			"x-ms-wmv": "wmv"
+		},
+		"application":
+		{
+			"json": "json",
+			"pdf": "pdf",
+			"javascript": "js",
+			"postcript": "ps",
+			"zip": "zip",
+			"x-gzip": "gz"
+		},
+		"text":
+		{
+			"css": "css",
+			"csv": "csv",
+			"html": "html",
+			"plain": "txt",
+			"xml": "xml"
+		}
+	}
+
+def random_mime_type(type=None):
+	'''
+	Returns a tuple of (mime_type, extension). Mime_type is a tuple containing
+	(supertype, subtype). If the type parameter is not None, the supertype will
+	be type.
+	'''
+
+	if type is None:
+		type = random.choice(self.mime_types.keys())
+
+	subtype = random.choice(self.mime_types[type].keys())
+
+	return (type, subtype), self.mime_types[type][subtype]
+
+def file_name():
+	'''
+	Creates a random file name, which is a random alphanumeric string with a
+	random number of characters, between 1 and 64, selected on a bell curve.
+	'''
+	return generate_name(64)
+
+def file(type = None):
+	'''
+	Creates a file object, which is a file name plus extension, and a mime type,
+	in a tuple. Both objects are strings.
+	'''
+
+	mime_type, extension = random_mime_type(type)
+
+	return '.'.join((file_name(), extension)), '/'.join(mime_type)
