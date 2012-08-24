b60b7c8819c66186104e8cf901901e4087c9bcd2
Nathan West <Lucretiel@gmail.com>
2012-08-24 18:06:20 -0400

    Fixed tests
    Fixed string not having quotes
    Added docstrings
    Changed test_random_names to be more pythonic
    test_random_names now uses a regular expression

--- mongotest/generators/names/test_directories.py
+++ mongotest/generators/names/test_directories.py
@@ -1,17 +1,33 @@
 import unittest
 import directories
-from names import char_set
+import re
 
 class TestDirectories(unittest.TestCase):
+    def setUp(self):
+        pass
+
     def test_random_names(self):
-        for _ in xrange(20):
-            name = directories.generate_name()
+        '''
+        Test the directories.generate_name function. Checks that the length is
+        correct, and that all the characters come from the alphanumeric characters
+        set.
+        '''
+
+        #Note that it is IMPOSSIBLE to determine with certainty that the
+        #generate_name function is perfoming perfectly. Instead, we repeat the
+        #sanity checks 100 times, giving a 1% chance of a false positive
+        for name in (directories.generate_name() for _ in xrange(100)):
+            #length is beteen 1 and 32
             self.assertLessEqual(len(name), 32)
             self.assertGreaterEqueal(len(name), 1)
-            for char in name:
-                self.assertIn(char, char_set)
+            self.assertIsNot(re.match('[a-zA-Z0-9]+', name), None)
 
     def test_random_heirarchies(self):
+        '''
+        Test the directories.directory_generator generator. Checks that the
+        heirarchy makes sense- that is, every directory has an enclosing
+        directory as well.
+        '''
         #create directories. For each one, confirm that the parent directory exists
         dir_generator = directories.directory_generator()
 
@@ -21,6 +37,6 @@ class TestDirectories(unittest.TestCase):
         self.assertIs(sample_directories[0], '/')
 
         for dir in sample_directories[1:]:
-            separator = dir.rfind(/, 0, -1)
+            separator = dir.rfind('/', 0, -1)
             containing_dir = dir[:separator]
             self.assertIn(containing_dir, sample_directories)
