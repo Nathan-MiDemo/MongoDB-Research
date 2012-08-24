197900fcf8d72620fa9ae74536effbfc7fb290f6
Nathan West <Lucretiel@gmail.com>
2012-08-24 13:34:57 -0400

    Added missing function generate_name

--- mongotest/generators/names/__init__.py
+++ mongotest/generators/names/__init__.py
@@ -1,1 +1,11 @@
 #This module contains all functions for generating strings and names
+
+import random
+import utility
+
+#todo: make this better
+char_set = 'abcdefghigklmnopqrstuvwkyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345679'
+
+def generate_name(length):
+	return ''.join(random.sample(char_set, utility.random_int_bell_curve(1, length)))
+	
