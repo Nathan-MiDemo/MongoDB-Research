a53ae355494f6d12560aa2098e8b7cce3c06664b
Nathan West <Lucretiel@gmail.com>
2012-08-27 17:42:37 -0400

    Revert "Clarity change to regular expressions"
    This reverts commit d9cdfff893014341fb51982752a03e05cc33f48a.

--- mongotest/generators/names/test_files.py
+++ mongotest/generators/names/test_files.py
@@ -47,7 +47,7 @@ class TestFiles(unittest.TestCase):
 
             self.assertLessEqual(len(name), 64)
             self.assertGreaterEqual(len(name), 1)
-            self.assertIsInstance(re.match("[a-zA-Z0-9]*", name), re.MatchObject)
+            self.assertIsNot(re.match("[a-zA-Z0-9]*", name), None)
 
     def test_full_file(self):
         file, mime_type = files.file()
