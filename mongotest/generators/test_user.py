70b1fbdfbf5263a636eed5117b8ffc2e988e487b
Nathan West <Lucretiel@gmail.com>
2012-08-29 12:31:11 -0400

    Added user generator consistency checks

---
+++ mongotest/generators/test_user.py
@@ -0,0 +1,39 @@
+import unittest
+import random
+import itertools
+import os.path as path
+
+import user
+
+class TestUser(user.unittest):
+    def test_user_random_consistency(self):
+        id = user.bson.ObjectID()
+
+        random.seed(2345)
+        list1 = list(itertools.islice(user.user_data_generator(100, id=id), 10))
+
+        random.seed(2345)
+        list2 = list(itertools.islice(user.user_data_generator(100, id=id), 10))
+
+        for item1, item2 in itertools.izip(list1, list2):
+            self.assertEqual(item1, item2)
+
+    def test_user_filename_consistency(self):
+        for file in itertools.islice(user.user_data_generator(100), 100):
+            url_name = path.basename(file['url'])
+            self.assertEqual(url_name, file['name'])
+
+    def test_user_OID_consistency(self):
+        generator1 = user.user_data_generator(100)
+        id1 = next(generator1)['accountId']
+
+        for file in islice(generator1, 10):
+            self.assertEqual(id1, file['accountOd'])
+
+        generator2 = user_data_generator(100)
+        id2 = next(generator2)['accountId']
+
+        self.assertNotEqual(id1, id2)
+        
+        for file in islice(generator2, 10):
+            self.assertEqual(id2, file['accountId'])
